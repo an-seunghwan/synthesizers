a88d05ab758c336f1b61e67c335ad11d3804a0bf
an-seunghwan <dpeltms79@gmail.com>
2023-05-22 10:35:22 +0900

    update datasets

---
+++ datasets/census.py
@@ -0,0 +1,50 @@
+#%%
+"""
+Source: https://archive.ics.uci.edu/ml/datasets/US+Census+Data+(1990)
+"""
+#%%
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import pandas as pd
+import numpy as np
+#%%
+import argparse
+def get_args(debug):
+    parser = argparse.ArgumentParser('parameters')
+    
+    parser.add_argument('--seed', type=int, default=1, 
+                        help='seed for repeatable results')
+    parser.add_argument('--train_length', type=int, default=60000, 
+                        help='length of train dataset')
+    parser.add_argument('--test_length', type=int, default=10000, 
+                        help='length of test dataset')
+    
+    if debug:
+        return parser.parse_args(args=[])
+    else:    
+        return parser.parse_args()
+#%%
+def main():
+    #%%
+    print("Data loading...")
+    config = vars(get_args(debug=False))
+    df_raw = pd.read_csv("../data/raw/USCensus1990.data.txt")
+    df_raw = df_raw.drop(columns="caseid")
+    #%%
+    print("Data splitting...")
+    np.random.seed(config["seed"])
+    idx = np.random.choice(
+        len(df_raw), 
+        config["train_length"] + config["test_length"], 
+        replace=False)
+    #%%
+    train = df_raw.iloc[idx[:config["train_length"]]].reset_index(drop=True)
+    test = df_raw.iloc[idx[config["train_length"]:]].reset_index(drop=True)
+    #%%
+    print("Data saving...")
+    train.to_csv("../data/census_train.csv")
+    test.to_csv("../data/census_test.csv")
+#%%
+if __name__ == '__main__':
+    main()
+#%%
---
+++ datasets/survey.py
@@ -0,0 +1,114 @@
+#%%
+"""
+- Source:
+https://www.kaggle.com/datasets/census/2013-american-community-survey
+- Codebook:
+https://www2.census.gov/programs-surveys/acs/tech_docs/pums/data_dict/PUMSDataDict13.txt
+"""
+#%%
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import pandas as pd
+import numpy as np
+#%%%
+def main():
+    #%%
+    print("Data loading...")
+    df = pd.read_csv('../data/raw/ss13husa.csv')
+    df = df[df["ST"] == 6] # .California/CA
+    df.reset_index().drop(columns=['index']).to_csv('../data/raw/survey.csv')
+    #%%
+    print("Data pre-processing...")
+    colnames = [
+        # "ACCESS", 
+        "ACR",
+        # "AGS",
+        "BATH",
+        "BLD",
+        "BROADBND",
+        "BUS",
+        "COMPOTHX",
+        "DIALUP",
+        "DSL",
+        "FIBEROP",
+        "FS",
+        "HANDHELD",
+        "HFL",
+        "LAPTOP",
+        "MODEM",
+        "OTHSVCEX",
+        "REFR",
+        "RWAT",
+        # "RWATPR",
+        "SATELLITE",
+        "SINK",
+        "STOV",
+        "TEL",
+        "TEN",
+        "TOIL",
+        "VEH",
+        "YBL",
+        "FES",
+        "FPARC",
+        "HHL",
+        "HHT",
+        "HUGCL",
+        "HUPAC",
+        "HUPAOC",
+        "HUPARC",
+        "KIT",
+        "LNGI",
+        "MULTG",
+        "MV",
+        "NOC",
+        "NPF",
+        "NPP",
+        "NR",
+        "NRC",
+        "PARTNER",
+        "PLM",
+        "PSF",
+        "R18",
+        "R60",
+        "R65",
+        "RESMODE",
+        "SRNT",
+        # "SSMC",
+        "SVAL",
+        "WIF",
+        "WKEXREL",
+        "WORKSTAT",
+    ]
+    """
+    eliminate column threshold: less than FSMOCP (75779 nan values)
+    """
+    with open("../data/survey_colnames.txt", "w") as f:
+        for s in colnames:
+            f.write(str(s) + "\n")
+    df[colnames].isna().sum(axis=0).sort_values()
+    
+    """dropna"""
+    df = df[colnames].dropna()
+    df = df.reset_index().drop(columns=['index'])
+    #%%
+    print("Data splitting...")
+    np.random.seed(1)
+    idx = np.random.choice(
+        len(df), 
+        len(df), 
+        replace=False)
+    #%%
+    train = df.iloc[idx[:60000]].reset_index(drop=True)
+    test = df.iloc[idx[60000:]].reset_index(drop=True)
+    #%%
+    print("Data saving...")
+    train.to_csv("../data/survey_train.csv")
+    test.to_csv("../data/survey_test.csv")
+    #%%
+    # for dis in colnames:
+    #     if len(df[dis].unique()) == 1:
+    #         print(dis)
+#%%
+if __name__ == "__main__":
+    main()
+#%%
